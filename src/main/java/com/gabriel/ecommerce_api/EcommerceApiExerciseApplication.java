e357323d0b7715c7f6faea6dc50f1a09f4b8a805
Gabriel Reyes <gbrielreyesb@gmail.com>
2026-09-11 09:55:31 -0300

    commitando a base do projeto inicial

---
+++ src/main/java/com/gabriel/ecommerce_api/EcommerceApiExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.ecommerce_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApiExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApiExerciseApplication.class, args);
+	}
+
+}
